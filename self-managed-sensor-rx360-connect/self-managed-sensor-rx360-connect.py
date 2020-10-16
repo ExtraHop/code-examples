6c08e5e41c1499c38439cb30d6e065cf8cc3fa03
Patrick Kreuch <patrickkreuch@extrahop.com>
2020-10-16 08:56:40 -0700

    Add self-managed sensor connect script

---
+++ self-managed-sensor-rx360-connect/self-managed-sensor-rx360-connect.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+  
+import requests
+import csv
+import json
+
+SENSORS_LIST = 'sensors.csv'
+
+# Read sensor hostnames and connection tokens from CSV
+sensors = []
+with open(SENSORS_LIST, 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        sensor = {
+            'host': row[0],
+            'api_key': row[1],
+            'token': row[2]
+        }
+        sensors.append(sensor)
+
+# Function that connects a sensor to CCP
+def connectSensor(sensor):
+    url = sensor['host'] + '/api/v1/cloud/connect'
+    headers = {'Authorization': 'ExtraHop apikey=%s' % sensor['api_key']}
+    data = {
+        "cloud_token": sensor['token']
+    }
+    r = requests.post(url, headers=headers, data=json.dumps(data))
+    if r.status_code == 201:
+        print('Successfully paired ' + sensor['host'])
+    else:
+        print('Error! Failed to pair ' + sensor['host'])
+        print(r.text)
+
+for sensor in sensors:
+    connectSensor(sensor)
