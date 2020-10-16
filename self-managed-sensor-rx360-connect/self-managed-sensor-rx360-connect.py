28d8735c394000b3d26a3cf1c9c014673f54f1cf
Patrick Kreuch <patrickkreuch@extrahop.com>
2020-10-16 10:34:23 -0700

    Add copyright to self managed sensor script

--- self-managed-sensor-rx360-connect/self-managed-sensor-rx360-connect.py
+++ self-managed-sensor-rx360-connect/self-managed-sensor-rx360-connect.py
@@ -1,5 +1,10 @@
+# COPYRIGHT 2020 BY EXTRAHOP NETWORKS, INC.
+#
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE', which is part of this source code package.
+
 #!/usr/bin/python3
-  
+
 import requests
 import csv
 import json
