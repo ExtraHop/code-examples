b87ae455d85ce7d71efacd37beca211f2f5e4a98
Marc Winners <marc@extrahop.com>
2020-12-15 17:12:56 -0800

    Add application metrics from V1 script
    X-Change-Id: a2a36f00e1e93cdb72b341b1b4a9de8b

--- sunburst/eh_dns.py
+++ sunburst/eh_dns.py
@@ -12,13 +12,131 @@ import urllib.request
 from pprint import pprint as pp
 
 
-MALICIOUS_URL_REGEX = (
+MALICIOUS_HOST_REGEX = (
     "/\\.(avsvmcloud|freescanonline|deftsecurity|thedoccloud|incomeupdate"
     "|zupertech|databasegalore|panhardware|websitetheme|highdatabase)\\.com$/"
 )
 
 if hasattr(ssl, "_create_unverified_context"):
     ssl._create_default_https_context = ssl._create_unverified_context
+
+
+def show_application_metrics(args):
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"ExtraHop apikey={args.api_key}",
+        "Content-Type": "application/json",
+    }
+
+    body = {
+        "cycle": args.cycle,
+        "from": args.from_time,
+        "until": args.until_time,
+        "metric_category": "dns_host_query_detail",
+        "metric_specs": [{"name": "req", "key1": f"{MALICIOUS_HOST_REGEX}"}],
+        "object_type": "application",
+        "object_ids": args.object_ids,
+    }
+
+    data = json.dumps(body).encode("utf-8")
+    req = urllib.request.Request(
+        f"https://{args.target}/api/v1/metrics", data=data, headers=headers
+    )
+    with urllib.request.urlopen(req) as resp:
+        resp_data = json.loads(resp.read().decode("utf-8"))
+
+    matches = []
+    for stat in resp_data["stats"]:
+        if stat["values"][0]:
+            host = stat["values"][0][0]["key"]["str"]
+            count = stat["values"][0][0]["value"]
+            if not count:
+                continue
+            matches.append({"host": host, "count": count})
+    if matches:
+        print("Found Sunburst activity in application metrics:")
+        pp(matches)
+    else:
+        print("No Sunburst activity found in application metrics.")
+
+
+def show_device_metrics(args):
+    """ Get all DNS Clients """
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"ExtraHop apikey={args.api_key}",
+        "Content-Type": "application/json",
+    }
+
+    body = {
+        "active_from": args.from_time,
+        "active_until": args.until_time,
+        "filter": {
+            "field": "activity",
+            "operand": "dns_client",
+            "operator": "=",
+        },
+    }
+    data = json.dumps(body).encode("utf-8")
+    req = urllib.request.Request(
+        f"https://{args.target}/api/v1/devices/search",
+        data=data,
+        headers=headers,
+    )
+    with urllib.request.urlopen(req) as resp:
+        clients = json.loads(resp.read().decode("utf-8"))
+
+    """ Initialize a device to dns request map. """
+    request_map = dict()
+    oids = []
+    for c in clients:
+        request_map[c["id"]] = {
+            "display_name": c["display_name"],
+            "ipaddr4": c["ipaddr4"],
+            "macaddr": c["macaddr"],
+            "matches": [],
+        }
+        oids.append(c["id"])
+
+    """ Get metrics for each dns client """
+    body = {
+        "cycle": args.cycle,
+        "from": args.from_time,
+        "until": args.until_time,
+        "metric_category": "dns_client",
+        "metric_specs": [
+            {"name": "host_query", "key1": f"{MALICIOUS_HOST_REGEX}"}
+        ],
+        "object_type": "device",
+        "object_ids": oids,
+    }
+    data = json.dumps(body).encode("utf-8")
+    req = urllib.request.Request(
+        f"https://{args.target}/api/v1/metrics",
+        data=data,
+        headers=headers,
+    )
+    with urllib.request.urlopen(req) as resp:
+        resp = json.loads(resp.read().decode("utf-8"))
+
+    """ Parse the stats and update the device to request map """
+    for stat in resp["stats"]:
+        if stat["values"][0]:
+            host = stat["values"][0][0]["key"]["str"]
+            count = stat["values"][0][0]["value"]
+            time = stat["time"]
+            request_map[stat["oid"]]["matches"].append(
+                {"time": time, "host": host, "count": count}
+            )
+
+    # Only show positive matches
+    request_map = {k: v for k, v in request_map.items() if v["matches"]}
+    """ print the request map """
+    if request_map:
+        print("Found Sunburst activity in device metrics:")
+        pp(request_map)
+    else:
+        print("No Sunburst activity found in device metrics.")
 
 
 def main():
@@ -75,78 +193,8 @@ def main():
         "default: %(default)s",
     )
     args = p.parse_args()
-
-    headers = {
-        "accept": "application/json",
-        "Authorization": f"ExtraHop apikey={args.api_key}",
-        "Content-Type": "application/json",
-    }
-
-    """ Get all DNS Clients """
-    body = {
-        "active_from": args.from_time,
-        "active_until": args.until_time,
-        "filter": {
-            "field": "activity",
-            "operand": "dns_client",
-            "operator": "=",
-        },
-    }
-    data = json.dumps(body).encode("utf-8")
-    req = urllib.request.Request(
-        f"https://{args.target}/api/v1/devices/search",
-        data=data,
-        headers=headers,
-    )
-    with urllib.request.urlopen(req) as resp:
-        clients = json.loads(resp.read().decode("utf-8"))
-
-    """ Initialize a device to dns request map. """
-    request_map = dict()
-    oids = []
-    for c in clients:
-        request_map[c["id"]] = {
-            "display_name": c["display_name"],
-            "ipaddr4": c["ipaddr4"],
-            "macaddr": c["macaddr"],
-            "matches": [],
-        }
-        oids.append(c["id"])
-
-    """ Get metrics for each dns client """
-    body = {
-        "cycle": args.cycle,
-        "from": args.from_time,
-        "until": args.until_time,
-        "metric_category": "dns_client",
-        "metric_specs": [
-            {"name": "host_query", "key1": f"{MALICIOUS_URL_REGEX}"}
-        ],
-        "object_type": "device",
-        "object_ids": oids,
-    }
-    data = json.dumps(body).encode("utf-8")
-    req = urllib.request.Request(
-        f"https://{args.target}/api/v1/metrics", data=data, headers=headers,
-    )
-    with urllib.request.urlopen(req) as resp:
-        resp = json.loads(resp.read().decode("utf-8"))
-
-    """ Parse the stats and update the device to request map """
-    for stat in resp["stats"]:
-        if stat["values"][0]:
-            host = stat["values"][0][0]["key"]["str"]
-            count = stat["values"][0][0]["value"]
-            time = stat["time"]
-            request_map[stat["oid"]]["matches"].append(
-                {"time": time, "host": host, "count": count}
-            )
-
-    # Only show positive matches
-    request_map = {k: v for k, v in request_map.items() if v["matches"]}
-
-    """ print the request map """
-    pp(request_map)
+    show_application_metrics(args)
+    show_device_metrics(args)
 
 
 if __name__ == "__main__":
